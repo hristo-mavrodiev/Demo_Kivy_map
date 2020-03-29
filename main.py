2b20bd3c760ed4663860814af4e07c55ca9bd44c
Hristo Mavrodiev <61916767+hristo-mavrodiev@users.noreply.github.com>
2020-03-29 22:41:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from kivy.garden.mapview import MapView, MapMarker, MapMarkerPopup
+from kivy.app import App
+from kivy.app import App
+from kivy.base import runTouchApp
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+
+
+class BGS2005Convertor(App):
+    def build(self):
+
+        layout = BoxLayout(orientation='vertical')
+        self.mapview = MapView(zoom=7, lat=42.6394, lon=25.057)
+        marker = MapMarkerPopup(id="first", lat=42.6394, lon=25.057)
+        marker.add_widget(BoxLayout(orientation='horizontal'))
+        self.mapview.add_marker(marker)
+        layout.add_widget(self.mapview)
+        b = BoxLayout(orientation='horizontal',
+                      height='32dp', size_hint_y=None)
+        b.add_widget(Button(text="Zoom in", on_press=lambda a: setattr(
+            self.mapview, 'zoom', self.mapview.zoom + 1)))
+        b.add_widget(Button(text="Zoom out", on_press=lambda a: setattr(
+            self.mapview, 'zoom', self.mapview.zoom - 1)))
+        layout.add_widget(b)
+        return layout
+
+if __name__ == "__main__":
+    BGS2005Convertor().run()
