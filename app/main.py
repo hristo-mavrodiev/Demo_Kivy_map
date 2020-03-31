e58420a0c2f35f36001801d73a455b01a5a7b974
hristo-mavrodiev <h.mavrodiev@abv.bg>
2020-03-31 20:26:11 +0300

    adding develop proj4

--- app/main.py
+++ app/main.py
@@ -11,6 +11,8 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 from kivy.uix.bubble import Bubble
 
+from libs.tr_proj import transform_epsg
+
 
 class EPSGConvertor(App):
     def build(self):
@@ -19,11 +21,11 @@ class EPSGConvertor(App):
         self.mapview = MapView(zoom=7, lat=42.6394, lon=25.057)
 
         bubble = Bubble(orientation="horizontal", padding=5)
-        text = "[b]Sample text here[/b]"
+        text = f" My coordinates:{transform_epsg('epsg:4326', 'epsg:3857', 42.6394, 25.057)}"
         label = Label(text=text, markup=True, halign="center")
         bubble.add_widget(label)
         marker = MapMarkerPopup(id="first", lat=42.6394,
-                                lon=25.057, popup_size=('150dp', '100dp'))
+                                lon=25.057, popup_size=('550dp', '100dp'))
         marker.add_widget(bubble)
         self.mapview.add_marker(marker)
 
@@ -34,8 +36,8 @@ class EPSGConvertor(App):
         b.add_widget(Button(text="Zoom out", on_press=lambda a: setattr(
             self.mapview, 'zoom', self.mapview.zoom - 1)))
 
-        layout.add_widget(b)
         layout.add_widget(self.mapview)
+        layout.add_widget(b)
         return layout
 
 
---
+++ app/tests/test_pyproj.py
@@ -0,0 +1,39 @@
+import unittest
+
+"""
+run the following command in main dir "app"
+python -m tests.test_pyproj
+
+simple "hack" to run directly from here
+# import os
+# import sys
+# cwd = os.getcwd()
+# sys.path.append(cwd + '/app/libs/')
+"""
+
+from libs.tr_proj import transform_epsg
+
+
+class PyprojTest(unittest.TestCase):
+    """
+    Test for transformation between different CRS.
+    """
+    def test_pyproj_transformation_mercator_to_WGS84(self):
+        """
+        Results from EPSG.io to test:
+        latitude, longtitude == North, East
+        42.56789, 23.45678 <==> 2611196.81, 5246429.88
+        transform_epsg('epsg:3857', 'epsg:4326', 2611196.81, 5246429.88)
+        transform_epsg('epsg:4326', 'epsg:3857', 42.56789, 23.45678)
+        """
+        n, e = transform_epsg('epsg:3857', 'epsg:4326', 2611196.81, 5246429.88)
+        self.assertEqual("{:.5f} {:.5f}".format(n, e), "42.56789 23.45678")
+
+    def test_pyproj_transformation_WGS84_to_merc(self):
+        lat, lon = transform_epsg('epsg:4326', 'epsg:3857', 42.56789, 23.45678)
+        self.assertEqual("{:.2f} {:.2f}".format(lat, lon), "2611196.81 5246429.88")
+
+
+if __name__ == "__main__":
+    print("success")
+    unittest.main()
